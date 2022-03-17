0e558b57b00fb1b6f9cd1e49e81d3442866854dc
Clarisse Scofield <clarisse.scofield@gmail.com>
2022-03-16 21:47:42 -0300

    Update impartialOfferings.py

--- codeJam/2021/impartialOfferings.py
+++ codeJam/2021/impartialOfferings.py
@@ -1,17 +1,17 @@
 # the problem here https://codingcompetitions.withgoogle.com/codejamio/round/00000000004360f2/0000000000777098#problem
 
-from collections import Counter
-
 def divide(pets, pets_size):
-    res = {}
-    for i in pets_size:
-        res[i] = pets_size.count(i)
-    print(res)
+    pets_size.sort()
+    anterior = -1
     valor = 1
     total_gasto = 0
-    for key, value in res.items():
-        total_gasto += value * valor
-        valor += 1
+    for pet in pets_size:
+        if(anterior < pet and anterior != -1):
+            valor += 1
+            total_gasto += valor
+        else:
+            total_gasto += valor
+        anterior = pet
         
     return total_gasto
     
